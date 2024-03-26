7464ca8e3c93f1a1f92febe606bba27c277bb0ac
andro-jr <prabin20panta@gmail.com>
2024-03-26 20:46:59 +0545

    product detail multiple images

--- js/script.js
+++ js/script.js
@@ -173,3 +173,50 @@ if (mobileMenuClose) {
     mobileMenu.classList.remove('show-mobile-nav');
   })
 }
+
+// Products details
+
+
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    }
+
+  ]
+});
